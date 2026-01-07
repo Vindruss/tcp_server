5636edfe83e83d361355e5a210a95b37792fdb58
Jan Kubala <kubala@argutec.eu>
2026-01-07 09:31:48 +0100

    oprava názvu

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-                'talker = tcp_server.robot_service:main',
+                'robot_service = tcp_server.robot_service:main',
         ],
 },
 )
